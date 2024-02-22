edb0672f92dcc57daaf59e25e4dfd482d3a0b60d
edas112 <you@example.com>
2024-02-22 22:50:40 +0200

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // components
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,26 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import { mockData } from '../mockData';
 export const AppContext = createContext();
 
 function AppContextProvider(props) {
-  const [data, setData] = useState(mockData);
-  const [cardData, setCardData] = useState([]);
-  const [favoriteData, setFavoriteData] = useState([]);
+  const [data, setData] = useState(
+    JSON.parse(localStorage.getItem('data')) || mockData
+  );
+  const [cardData, setCardData] = useState(
+    JSON.parse(localStorage.getItem('cardData')) || []
+  );
+  const [favoriteData, setFavoriteData] = useState(
+    JSON.parse(localStorage.getItem('favoriteData')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(data));
+    localStorage.setItem('cardData', JSON.stringify(cardData));
+  }, [data, cardData]);
+
+  useEffect(() => {
+    localStorage.setItem('favoriteData', JSON.stringify(favoriteData));
+  }, [favoriteData]);
 
   const addToCard = (item) => {
     setCardData([...cardData, item]);
@@ -17,6 +32,7 @@ function AppContextProvider(props) {
 
   const removeFromCard = (item) => {
     setData([...data, item]);
+
     const filteredData = cardData.filter(
       (dataItem) => dataItem.title !== item.title
     );
