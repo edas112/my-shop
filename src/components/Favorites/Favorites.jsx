51b1d4292acc902fa072d5c0f84a3c2ca4005e55
edas112 <you@example.com>
2024-02-27 20:05:52 +0200

    mycard input

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,25 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 // components
 import Card from '../Card/Card';
 import { AppContext } from '../../context/AppContext';
 
 function Favorites() {
+  const [searchValue, setSearchValue] = useState('');
   const { favoriteData } = useContext(AppContext);
   console.log(favoriteData);
   return (
     <main className="main-conteiner">
-      {favoriteData.map((item) => {
-        return (
-          <Card
-            key={item.title}
-            title={item.title}
-            description={item.description}
-            img={item.img}
-            buttonFunk={() => {}}
-          />
-        );
-      })}
+      <div className="conteiner-input">
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
+      </div>
+      {favoriteData
+        .filter(
+          ({ title, description }) =>
+            title.toLowerCase().includes(searchValue) ||
+            description.toLowerCase().includes(searchValue.toLocaleLowerCase())
+        )
+        .map((item) => {
+          return (
+            <Card
+              key={item.title}
+              title={item.title}
+              description={item.description}
+              img={item.img}
+              buttonFunk={() => {}}
+            />
+          );
+        })}
     </main>
   );
 }
--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -1,23 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Card from '../Card/Card';
 import { AppContext } from '../../context/AppContext';
 
 function MyCard() {
+  const [searchValue, setSearchValue] = useState('');
   const { cardData, removeFromCard } = useContext(AppContext);
   return (
     <main className="conteiner">
-      {cardData.map(({ title, description, img }) => {
-        return (
-          <Card
-            key={title}
-            title={title}
-            description={description}
-            img={img}
-            buttonFunk={removeFromCard}
-            card={true}
-          />
-        );
-      })}
+      <div className="conteiner-input">
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
+      </div>
+      {cardData
+        .filter(
+          ({ title, description }) =>
+            title.toLowerCase().includes(searchValue) ||
+            description.toLowerCase().includes(searchValue.toLocaleLowerCase())
+        )
+        .map(({ title, description, img }) => {
+          return (
+            <Card
+              key={title}
+              title={title}
+              description={description}
+              img={img}
+              buttonFunk={removeFromCard}
+              card={true}
+            />
+          );
+        })}
     </main>
   );
 }
